fbb747df554f21b0f79e3a4abc61feeed6cd140c
林聖泰 <shengtai0201@gmail.com>
2020-11-08 17:53:39 +0800

    chapter 2

--- Chapter2Tests.java
+++ Chapter2Tests.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.*;
+import java.util.Random;
 
 @DisplayName("第一章：陣列")
 public class Chapter2Tests {
@@ -95,5 +96,74 @@ public class Chapter2Tests {
 
         this.merge(a, b, c);
         this.show(c);
+    }
+
+    private int[] RndSort() {
+        Random rnd = new Random();
+
+        final int n = 42;
+        int idx[] = new int[n];
+        int loto[] = new int[6];
+
+        for (int i = 0; i < n; i++)
+            idx[i] = 0;
+
+        for (int i = 0; i < 6; i++) {
+            int k;
+
+            do {
+                k = rnd.nextInt(n);
+            } while (idx[k] == 1);
+
+            idx[k] = 1;
+        }
+
+        for (int i = 0, j = 0; i < n; i++)
+            if (idx[i] == 1)
+                loto[j++] = i + 1;
+
+        return loto;
+    }
+
+    @DisplayName("產生樂透號碼")
+    @Test
+    void test5() {
+        int loto[] = this.RndSort();
+
+        for (int i = 0; i < loto.length; i++)
+            System.out.print(loto[i] + "\t");
+    }
+
+    private int[][] magicSquare(int n) {
+        int magic[][] = new int[n][n];
+
+        int k = 1, row = 0, column = n / 2;
+        while (k <= n * n) {
+            magic[row][column] = k++;
+
+            int r = (row + n - 1) % n;
+            int c = (column + 1) % n;
+            if (magic[r][c] == 0) {
+                row = r;
+                column = c;
+            } else
+                row++;
+        }
+
+        return magic;
+    }
+
+    @DisplayName("魔術方陣")
+    @Test
+    void test6() {
+        int magic[][] = this.magicSquare(7);
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++)
+                System.out.print(magic[i][j] + "\t");
+
+            System.out.println();
+        }
+
     }
 }
